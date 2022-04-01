c01cf21c660d320d8efb6f20c81ca6d7f4064de4
Daniel Oliva <danieloliva20902@gmail.com>
2022-04-01 11:50:29 +0200

    Creacion del proyecto

---
+++ FootageBackend/src/main/java/com/danieloliva/FootageBackend/FootageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.danieloliva.FootageBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootageBackendApplication.class, args);
+	}
+
+}
---
+++ FootageBackend/src/test/java/com/danieloliva/FootageBackend/FootageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieloliva.FootageBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
